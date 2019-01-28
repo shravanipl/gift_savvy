f829c0af1206c75fcd84e8e2b16efa2cce569209
shravanipl <pl.shravani@gmail.com>
2019-01-27 20:08:23 -0600

    Changed API base URL

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-	process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
+	process.env.REACT_APP_API_BASE_URL || 'https://gift-savvy-server.herokuapp.com/api';
 
 export const GIFT_SEARCH_API_URL = process.env.REACT_APP_GIFT_SEARCH_API_URL || 'https://rest.viglink.com/api/product/search';
 export const GIFT_SEARCH_API_KEY = '743c9f270211c6ba265e5512940de096';
